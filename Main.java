a886faa636f6ede696533062721366b38ff4cf98
verica-kar <vericakaranakova@gmail.com>
2024-03-19 18:14:49 -0500

    save

--- Main.java
+++ Main.java
@@ -2,11 +2,13 @@
 // CS 458 Introduction to Information Security
 // Assignment 2
 
-package assignment2;
+package assignment2.cs458assignment2;
 
 import java.util.Scanner;
 
 public class Main {
+
+    static String ciphertext = "";
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -40,6 +42,7 @@ public class Main {
                 } else {
                     System.out.println(shiftCipher(plaintext, 3));
                 }
+                break;
 
             case 2:
                 System.out.println("Please Enter your plaintext: ");
@@ -48,20 +51,38 @@ public class Main {
                 yesOrNo = sc2.nextLine();
 
                 if(yesOrNo.contains("Y")){
-                    System.out.println("Please enter your key: ");
+                    System.out.println("Please enter your key by rearraging 12345: ");
                     key = sc.nextInt();
                     System.out.println(permutationCipher(plaintext, key));
                 } else {
-                    System.out.println(permutationCipher(plaintext, 3));
+                    System.out.println(permutationCipher(plaintext, 45132));
                 }
+                break;
         
             case 3:
-                System.out.println("Please Enter your plaintext: ");
+                System.out.println("Please Enter your plaintext (must be greater than 16 letters): ");
                 plaintext = sc2.nextLine();
+                System.out.println("Would you like to enter a key? Y/N ");
+                yesOrNo = sc2.nextLine();
+
+                if(yesOrNo.contains("Y")){
+                    System.out.println("Please enter your key (must be <= 4): ");
+                    key = sc.nextInt();
+                    System.out.println(simpleTransposition(plaintext, key));
+                } else {
+                    System.out.println(simpleTransposition(plaintext, 2));
+                }
+                break;
 
             case 4:
                 System.out.println("Please Enter your plaintext: ");
                 plaintext = sc2.nextLine();
+                break;
+
+            case 5:
+                System.out.println("Please Enter your plaintext: ");
+                plaintext = sc2.nextLine();
+                break;
 
             default:
                 break;
@@ -69,28 +90,65 @@ public class Main {
     }
 
     public static String shiftCipher(String pt, int k){
-        String ct = "";
-        return ct;
+        for(int i = 0; i < pt.length(); i++){
+            char let = pt.charAt(i);
+            if(let == ' '){
+                ciphertext += " ";
+            } else if((let >= 'A') && (let <= 'Z')) {
+                if((let + k) > 90){
+                    let += k;
+                    let -= 26;
+                } else {
+                    let += k;
+                }
+            } else if((let >= 'a') && (let <= 'z')){
+                if((let + k) > 122) {
+                    let += k;
+                    let -= 26;
+                } else {
+                    let += k;
+                }
+            } else {
+                return("Invalid charaters used.");
+            }
+            ciphertext += String.valueOf(let);
+        }
+        return ciphertext;
     }
 
     public static String permutationCipher(String pt, int k){
-        String ct = "";
-        return ct;
+        return ciphertext;
     }
 
     public static String simpleTransposition(String pt, int k){
-        String ct = "";
-        return ct;
+        String ciphertext = "";
+        char[][] matrix = new char[k][k];
+        int counter = 0;
+
+        while(counter < (pt.length() - 1)){
+            for(int i = 0; i < k; i++){
+                for(int j = 0; j < k; j++){
+                    matrix[i][j] = pt.charAt(counter);
+                    counter++;
+                }
+            }
+
+            for(int i = 0; i < k; i++){
+                for(int j = 0; j < k; j++){
+                    ciphertext += String.valueOf(matrix[j][i]);
+                }
+            }
+        }
+
+        return ciphertext;
     }
 
     public static String doubleTransposition(String pt, int k){
-        String ct = "";
-        return ct;
+        return ciphertext;
     }
 
     public static String vigenereCipher(String pt, int k){
-        String ct = "";
-        return ct;
+        return ciphertext;
     }
 
 }
